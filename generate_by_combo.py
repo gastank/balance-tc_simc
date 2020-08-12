cb654278499b08b96dcc41a22fcf7b4bb47463e5
gastank <42421688+gastank@users.noreply.github.com>
2020-08-12 16:52:49 -0700

    .

---
+++ generate_by_combo.py
@@ -0,0 +1,72 @@
+import requests
+import time
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('apikey', type=str, help='raidbots apikey')
+parser.add_argument('-t', '--targets', type=int, nargs='?', default=1, const=1, help='set desired sim targets')
+args = parser.parse_args()
+apikey = args.apikey
+targets = str(args.targets)
+
+post_url = 'https://mimiron.raidbots.com/sim'
+get_url = 'https://mimiron.raidbots.com/api/job/'
+report_url = 'https://mimiron.raidbots.com/simbot/report/'
+
+profile = apl = sets = ""
+
+covs = ['kyrian', 'night_fae', 'venthyr', 'necrolord']
+legs = {'oneth':7087, 'pulsar':7088, 'dream':7108, 'lycaras':7110}
+
+with open('sandbag.txt', 'r') as fp:
+    profile = fp.read()
+
+with open('balance.txt', 'r') as fp:
+    apl = fp.read()
+
+with open('talent_profiles.txt', 'r') as fp:
+    sets = fp.read()
+
+buffer = {}
+
+for cov in covs:
+    for leg, bonus in legs.items():
+        name = cov + ' - ' + leg
+        simc = profile + '\ntarget_error=0.1\ndesired_targets=' + targets + '\n\ntalents=0000000\ncovenant=' + cov + '\n\ntabard=,id=31405,bonus_id=' + str(bonus) + '\n\nname=\"' + name + '\"\n\n' + apl + sets
+
+        post = requests.post(post_url, json={'type': 'advanced', 'apiKey': apikey, 'advancedInput': simc})
+        reply = post.json()
+        simID = reply['simId']
+
+        sim_url = report_url + simID
+        print(sim_url)
+
+        while True:
+            get = requests.get(get_url + simID)
+            status = get.json()
+            time.sleep(5)
+            if (status['job']['state'] == 'complete'):
+                break
+
+        data = requests.get(sim_url + '/data.json')
+        results = data.json()
+
+        dps_list = {}
+        for actor in results['sim']['profilesets']['results']:
+            dps_list[actor['name']] = actor['mean']
+
+        dps_max = max(dps_list, key=dps_list.get)
+        name2 = cov.rjust(9,'_') + '-' + leg.ljust(7,'_')
+        html = '<div><a href=\"chart.html?simid=' + simID + '\" target=\"simframe\">' + name2.replace('_', '&nbsp;') + '|' + str(dps_max).replace('_', '&nbsp;') + ' ' + f'{dps_list[dps_max]:.2f}' + '</a></div>\n'
+        buffer[html] = dps_list[dps_max]
+
+sorted_buf = sorted(buffer.items(), key=lambda x: x[1], reverse=True)
+
+betabot = open('by_combo_' + targets + '.html', 'w')
+betabot.write('<html><style>body {margin-left:0; margin-right:0} a {color:#FF7D0A; text-decoration:none; font-family:monospace; font-size:large;}</style><body>\n')
+
+for buf in sorted_buf:
+    betabot.write(buf[0])
+
+betabot.write('</body></html>\n')
+betabot.close()
---
+++ generate_by_talent.py
@@ -0,0 +1,78 @@
+import requests
+import time
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('apikey', type=str, help='raidbots apikey')
+#parser.add_argument('-t', '--targets', type=int, nargs='?', default=1, const=1, help='set desired sim targets')
+args = parser.parse_args()
+apikey = args.apikey
+#with open('apikey', 'r') as fp: apikey = fp.read()
+#targets = str(args.targets)
+
+post_url = 'https://mimiron.raidbots.com/sim'
+get_url = 'https://mimiron.raidbots.com/api/job/'
+report_url = 'https://mimiron.raidbots.com/simbot/report/'
+
+profile = apl = combo = sets = ""
+jso = {}
+
+with open('sandbag.txt', 'r') as fp:
+    profile = fp.read()
+
+with open('balance.txt', 'r') as fp:
+    apl = fp.read()
+
+with open('leg_x_cov.txt', 'r') as fp:
+    combo = fp.read()
+
+sets = open('talent_profiles.txt', 'r')
+betabot = open('by_talent.html', 'w')
+betabot.write('<html><script>function newlink(e) {if (e.target.tagName === \'A\') {parent.setTitle(e.target.textContent);}} document.addEventListener(\'click\'", newlink, false);</script><style>body {margin-left:0; margin-right:0} a {color:#FF7D0A; text-decoration:none; font-family:monospace; font-size:large;}</style><body><div style=\"columns:5;\"><div style=\"display:inline-block;\">\n')
+
+for line in sets:
+    if line == '\n':
+        html = '</div><div style=\"display: inline-block;\">\n'
+        betabot.write(html)
+        continue
+
+    splits = line.split('=', 1)
+    talent = splits[-1]
+    name = splits[0].split('.')[-1]
+    simc = profile + '\ntarget_error=0.1\n' + talent + '\n' + apl + '\n' + 'name=' + name + '\n\n' + combo
+
+    post = requests.post(post_url, json={'type': 'advanced', 'apiKey': apikey, 'advancedInput': simc})
+    reply = post.json()
+    simID = reply['simId']
+
+    sim_url = report_url + simID
+    print(sim_url)
+
+    while True:
+        get = requests.get(get_url + simID)
+        status = get.json()
+        time.sleep(5)
+        if (status['job']['state'] == 'complete'):
+            break
+
+    data = requests.get(sim_url + '/data.json')
+    result = data.json()
+
+    dps_list = []
+    for actor in result['sim']['players']:
+        dps_list.append(actor['collected_data']['dps']['mean'])
+
+    dps_max = max(dps_list)
+    name = name.strip('\"') + ' ' + f'{dps_max:.2f}'
+    sim_url = sim_url + '/index.html'
+    #html = '<div><a href="' + sim_url + '">' + name.replace('_','&nbsp;') + ' ' + f'{dps_max:.2f}' + '</a></div>\n'
+    html = '<div><a href="' + sim_url + '">' + name + '</a></div>\n'
+    betabot.write(html)
+    jso[name] = simID
+
+betabot.write('</div></div></body></html>\n')
+betabot.close()
+sets.close()
+
+with open('jso.js', 'w') as fp:
+    fp.write('var jso =' + str(jso) + ';')
